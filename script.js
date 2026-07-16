c8e081be9fcecda4081dd7d343551a3d15fb43c8
Gabriel Mallare <mallaregabriel9@gmail.com>
2026-07-16 10:03:47 +0800

    Completed project requirements

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 function showMessage() {
-    alert("Welcome to my website!");
+    alert("Thank you for visiting our website!");
+}
+
+window.onload = function () {
+
+    const greeting = document.getElementById("greeting");
+
+    if(greeting){
+
+        const hour = new Date().getHours();
+
+        if(hour < 12){
+            greeting.innerHTML = "Good Morning!";
+        }
+        else if(hour < 18){
+            greeting.innerHTML = "Good Afternoon!";
+        }
+        else{
+            greeting.innerHTML = "Good Evening!";
+        }
+    }
+}
+
+function validateForm(){
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if(name === "" || email === "" || message === ""){
+        alert("Please fill out all fields.");
+        return false;
+    }
+
+    alert("Form submitted successfully!");
+    return true;
 }
